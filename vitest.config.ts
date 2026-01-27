0aa4070f9cd5b06df000c60e2c1960bc9975dd20
Terence Peng <terencep429@gmail.com>
2026-01-27 18:11:52 +1100

    build: infrastructure and config updates

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,10 @@ export default defineConfig({
   test: {
     environment: 'node', // We're testing node-side logic mostly, browser interactions are via Playwright
     testTimeout: 30000,
+    globals: true,
+    include: ['**/*.test.ts'],
+  },
+  esbuild: {
+    target: 'node18',
   },
 });
